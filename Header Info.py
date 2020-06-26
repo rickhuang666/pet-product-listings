d51f4da5290ad0a1d97d4fee8cb90e9a909dbd2b
Rick Huang <rickhuang666@gmail.com>
2020-06-26 15:54:00 -0400

    Update Header

--- Header Info.py
+++ Header Info.py
@@ -4,3 +4,4 @@ import time
 from datetime import date
 from bs4 import BeautifulSoup
 from selenium import webdriver
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0'}
